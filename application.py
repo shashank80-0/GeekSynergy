de1bb3985d189e821b057423c6d36fab4f2af9bc
Shashank Agarwal <63952954+shashank80-0@users.noreply.github.com>
2020-07-18 23:27:11 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -86,6 +86,10 @@ def info():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
         
 if __name__ =='__main__':
     app.run()
